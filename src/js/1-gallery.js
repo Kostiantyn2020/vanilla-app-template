fe7963b2ea8f2a3efe213e19437ed2e45630e6cc
Kostiantyn2020 <161701157+Kostiantyn2020@users.noreply.github.com>
2024-07-11 11:41:37 +0300

    2

---
+++ src/js/1-gallery.js
@@ -0,0 +1,15 @@
+.gallery {
+  list-style: none;
+  display: flex;
+  flex-wrap: wrap;
+  gap: 10px;
+}
+
+.gallery-item {
+  flex-basis: calc(33.333% - 10px);
+}
+
+.gallery-image {
+  width: 100%;
+  height: auto;
+}
